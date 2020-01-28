fa049746e2f4d909c88d30707f12117b050c4869
kimetix99 <qpico99@gmail.com>
2020-01-28 14:18:13 +0100

    refactoring

--- Pawn.py
+++ Pawn.py
@@ -9,6 +9,30 @@ class Pawn(Pice):
     def get_possible_moves(board):
         moves=[]
         self.move_forward(moves, board)
+        self.kill_move(moves, board)
+        return moves
 
-    def move_forward(moves, board)
-        pass
+    def move_forward(moves, board):
+        if self.init_pos:
+            moves.append(Move((self.posX, self.posY), (self.posX, self.check_side(self.side, self.posY, 2))))
+            moves.append(Move((self.posX, self.posY), (self.posX, self.check_side(self.side, self.posY, 1))))
+        else:
+            moves.append(Move((self.posX,self.posY), (self.posX,self.check_side(self.side, self.posY, 1))))
+
+    def kill_move(moves, board):
+        if self.can_kill_left(board):
+            moves.append(Move((self.posX, self.posY), (self.posX-1, self.check_side(self.side, self.posY, 1))))
+        if self.can_kill_right(board):
+            moves.append(Move((self.posX, self.posY), (self.posX+1, self.check_side(self.side, self.posY, 1))))
+
+    def can_kill_left(board):
+        return board.board[posX-1][self.check_side(self.side, self.posY,1)]['p'] != ''
+
+    def can_kill_right(board):
+        return board.board[posX+1][self.check_side(self.side, self.posY,1)]['p'] != ''
+    
+    def check_side(side,pos,value):
+        if side == 'black': 
+            return pos-value 
+        else: 
+            return pos+value 
