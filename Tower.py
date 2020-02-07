bd1dfe350dfd61bae4f86d7e18a0a6a3c5e09fb2
kimetix99 <qpico99@gmail.com>
2020-02-08 00:12:03 +0100

    arreglat moviment torre

--- Tower.py
+++ Tower.py
@@ -20,21 +20,29 @@ class Tower(Pice):
         while next_posY >= 0 and board[next_posY][self.posX]['p'] == '':
             moves.append(Move((self.posX,self.posY),(self.posX,next_posY)))
             next_posY-=1
+        if next_posY >= 0 and board[next_posY][self.posX]['p'].side != self.side:
+            moves.append(Move((self.posX,self.posY),(self.posX,next_posY)))
 
     def move_left(self, moves, board):
         next_posX = self.posX-1
         while next_posX >= 0 and board[self.posY][next_posX]['p'] == '':
             moves.append(Move((self.posX,self.posY),(next_posX,self.posY)))
             next_posX-=1
+        if next_posX >= 0 and board[self.posY][next_posX]['p'].side != self.side:
+            moves.append(Move((self.posX,self.posY),(next_posX,self.posY)))
 
     def move_right(self, moves, board):
         next_posX = self.posX+1
         while next_posX <= 7 and board[self.posY][next_posX]['p'] == '':
             moves.append(Move((self.posX,self.posY),(next_posX,self.posY)))
             next_posX+=1
+        if next_posX <= 7 and board[self.posY][next_posX]['p'].side != self.side:
+            moves.append(Move((self.posX,self.posY),(next_posX,self.posY)))
 
     def move_down(self, moves, board):
         next_posY = self.posY+1
         while next_posY <= 7 and board[next_posY][self.posX]['p'] == '':
             moves.append(Move((self.posX,self.posY),(self.posX,next_posY)))
             next_posY+=1
+        if next_posY <= 7 and board[next_posY][self.posX]['p'].side != self.side:
+            moves.append(Move((self.posX,self.posY),(self.posX,next_posY)))
